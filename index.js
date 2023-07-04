c262209fad59d14c8b9122ac35d89acf488a4c98
root <nagarajunaresh83@gmail.com>
2023-07-04 11:27:36 +0000

    answer

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  // Write your code here.
+  <div class="container">
+    <h1 class="heading">Congratulations</h1>
+    <div class="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h3 class="name">Kiran V</h3>
+      <p class="para">
+        Vishnu Institute of Computer Education and Technology.
+        <br />
+        Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
