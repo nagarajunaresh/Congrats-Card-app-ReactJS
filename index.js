416f1578d74af27d36c328ad2537e0eea911af90
nagarajunaresh <nagarajunaresh83@gmail.com>
2023-11-01 04:45:21 +0000

    solution1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const element = (
   // Write your code here.
-  <div class="container">
-    <h1 class="heading">Congratulations</h1>
-    <div class="card-container">
+  <div className="container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card-container">
       <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
-      <h3 class="name">Kiran V</h3>
-      <p class="para">
+      <h3 className="name">Kiran V</h3>
+      <p className="para">
         Vishnu Institute of Computer Education and Technology.
         <br />
         Bhimavaram
